8c931ab3374df4d07b668f0964030abb807c1bfe
Lois Abifoluwa Adelaja <adelajalois95@gmail.com>
2024-10-20 18:39:51 +0100

    Update PrinterApp.java

--- Programming Assignment/printer app/src/PrinterApp.java
+++ Programming Assignment/printer app/src/PrinterApp.java
@@ -17,17 +17,23 @@ public class PrinterApp {
         System.out.println("The value of number1 is " + number1);
         System.out.println("The value of number2 is " + number2);
 
-        //multiply the numbers
+        // multiply the numbers
         
         multiplication = number1 * number2;
         System.out.println(number1 + " * " + number2 + " = " + multiplication);
 
+        // addition of the numbers 
+        
         addition = number1 + number2;
         System.out.println(number1 + " + " + number2 + " = " + addition);
 
+        // division of the numbers 
+        
         division = (double) number1 / number2;
         System.out.println(number1 + " / " + number2 + " = " + division);
 
+        // subtraction of the numbers 
+        
         subtraction = number1 - number2;
         System.out.println(number1 + " - " + number2 + " = " + subtraction);
     }
